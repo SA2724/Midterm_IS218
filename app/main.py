cd719ca61bc87715cc9d0c7c642ba70b8b78b439
SA2724 <sa2724@njit.edu>
2024-11-01 19:07:05 -0400

    added subtraction function and test

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 # addition Function 
 def addition(a,b):
     return a + b
+
+def subtraction(a,b):
+    return a - b 
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,7 @@ from app.main import addition
 def test_addition():
     '''Addition Function'''
     assert addition(1,1) == 2
+
+def test_subtraction()
+    '''Subtraction Function'''
+    assert subtraction(1,1) == 0
